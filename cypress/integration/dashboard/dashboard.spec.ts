f7250e83be3e6664f88a475c5bfe12cfcfdb3d3b
David Badalyan <david.b@fifth-llc.com>
2020-03-21 15:31:37 +0400

    test: doc's approche of testing

---
+++ cypress/integration/dashboard/dashboard.spec.ts
@@ -0,0 +1,32 @@
+describe('DashboardModule', () => {
+  beforeEach(() => {
+    cy.login('user', '123');
+    cy.visit('dashboard');
+  });
+
+  it('should have Menu button', () => {
+    cy.get('button[smpButton]').should('contain', 'Dashboard Menu');
+  });
+
+  it('should open popover menu', () => {
+    cy.get('button[smpButton]').click()
+      .get('smp-popover').should('be.visible');
+  });
+
+  it('should have popover with "Go to Home" item', () => {
+    cy.get('button[smpButton]').click()
+      .get('smp-popover').should('contain', 'Go to Home');
+  });
+
+  it('should display spinner when "Go to Home" is clicked', () => {
+    cy.get('button[smpButton]').click()
+      .get('smp-popover').contains('Go to Home').click()
+      .get('smp-spinner').should('be.visible');
+  });
+
+  it('should navigate to "/home" when "Go to Home" is clicked', () => {
+    cy.get('button[smpButton]').click()
+      .get('smp-popover').contains('Go to Home').click();
+    cy.on('url:changed', () => cy.url().should('contain', 'home'));
+  });
+});
